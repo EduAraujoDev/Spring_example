ac9f153fb018dda40135998dfa528b22739c4890
Eduardo Araújo <eduardo.araujo0@outlook.com>
2020-03-24 12:57:03 -0300

    Alterado para buscar os topicos do banco mysql

--- src/main/java/br/com/alura/forum/controller/TopicController.java
+++ src/main/java/br/com/alura/forum/controller/TopicController.java
@@ -5,6 +5,8 @@ import br.com.alura.forum.model.Category;
 import br.com.alura.forum.model.Course;
 import br.com.alura.forum.model.User;
 import br.com.alura.forum.model.topic.domain.Topic;
+import br.com.alura.forum.repository.TopicRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,15 +19,12 @@ import java.util.List;
 @RestController
 public class TopicController {
 
+    @Autowired
+    private TopicRepository topicRepository;
+
     @GetMapping(value = "/api/topics", produces = MediaType.APPLICATION_JSON_VALUE)
     public List<TopicBriefOutputDto> listTopics() {
 
-        Category subcategory = new Category("Java", new Category("Programação"));
-        Course course = new Course("Java e JSF", subcategory);
-        Topic topic = new Topic("Problemas com o JSF",
-                "Erro so fazer conversão da data",
-                new User("Fulano", "fulano@gmail.com", "123456"), course);
-
-        return TopicBriefOutputDto.listFromTopics(Arrays.asList(topic, topic, topic));
+        return TopicBriefOutputDto.listFromTopics(topicRepository.findAll());
     }
 }
---
+++ src/main/java/br/com/alura/forum/repository/TopicRepository.java
@@ -0,0 +1,15 @@
+package br.com.alura.forum.repository;
+
+import br.com.alura.forum.model.topic.domain.Topic;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.Repository;
+
+import java.util.List;
+
+public interface TopicRepository extends Repository<Topic, Long> {
+
+    @Query("select t from Topic t")
+    List<Topic> list();
+
+    List<Topic> findAll();
+}
