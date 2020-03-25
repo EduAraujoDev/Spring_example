74d3fa002c5f0777636c9d2050730f3d2189189b
Eduardo Araújo <eduardo.araujo0@outlook.com>
2020-03-25 13:50:43 -0300

    Usando specification do spring data jpa para usar filtro

--- src/main/java/br/com/alura/forum/controller/TopicController.java
+++ src/main/java/br/com/alura/forum/controller/TopicController.java
@@ -1,5 +1,6 @@
 package br.com.alura.forum.controller;
 
+import br.com.alura.forum.dto.input.TopicSearchInputDto;
 import br.com.alura.forum.dto.output.TopicBriefOutputDto;
 import br.com.alura.forum.model.Category;
 import br.com.alura.forum.model.Course;
@@ -7,6 +8,7 @@ import br.com.alura.forum.model.User;
 import br.com.alura.forum.model.topic.domain.Topic;
 import br.com.alura.forum.repository.TopicRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,8 +25,9 @@ public class TopicController {
     private TopicRepository topicRepository;
 
     @GetMapping(value = "/api/topics", produces = MediaType.APPLICATION_JSON_VALUE)
-    public List<TopicBriefOutputDto> listTopics() {
+    public List<TopicBriefOutputDto> listTopics(TopicSearchInputDto topicSearch) {
 
-        return TopicBriefOutputDto.listFromTopics(topicRepository.findAll());
+        Specification<Topic> topicSearchSpecification = topicSearch.build();
+        return TopicBriefOutputDto.listFromTopics(topicRepository.findAll(topicSearchSpecification));
     }
 }
