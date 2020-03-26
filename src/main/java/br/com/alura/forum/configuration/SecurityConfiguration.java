7fa45c4c64ed8ff5576fe1838221f2817a6f8c1d
Eduardo Araújo <eduardo.araujo0@outlook.com>
2020-03-26 17:46:56 -0300

    Liberação de acesso public a alguns endpoints - spring security

--- src/main/java/br/com/alura/forum/configuration/SecurityConfiguration.java
+++ src/main/java/br/com/alura/forum/configuration/SecurityConfiguration.java
@@ -3,7 +3,9 @@ package br.com.alura.forum.configuration;
 import br.com.alura.forum.security.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -19,5 +21,16 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.userDetailsService(userService)
                 .passwordEncoder(new BCryptPasswordEncoder());
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers(HttpMethod.GET, "/api/topics/**").permitAll()
+                .anyRequest().authenticated()
+            .and()
+                .formLogin()
+            .and()
+                .httpBasic();
     }
 }
