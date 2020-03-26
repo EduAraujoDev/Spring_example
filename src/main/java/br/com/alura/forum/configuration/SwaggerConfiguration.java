48e21ae7097d461898e235a7f5a9ddf87c14a50a
Eduardo Araújo <eduardo.araujo0@outlook.com>
2020-03-26 12:36:10 -0300

    Habilitando o springfox swagger 2

---
+++ src/main/java/br/com/alura/forum/configuration/SwaggerConfiguration.java
@@ -0,0 +1,36 @@
+package br.com.alura.forum.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfiguration {
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("br.com.alura.forum"))
+                .paths(PathSelectors.ant("/api/**"))
+                .build()
+                .apiInfo(apiInfo());
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("Alura forum API documentation")
+                .description("Está é a documentação interatica da Rest API do Fórum da Alura. Tente enviar alum request ;)")
+                .version("1.0")
+                .contact(new Contact("Alura", "https://cursos.alura.com.br/", "contato@alura.br"))
+                .build();
+    }
+}
