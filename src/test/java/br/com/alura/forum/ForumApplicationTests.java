1930d7f6654003e2d78c1c8cff3f52e0cac7dcb2
Eduardo Araújo <eduardo.araujo0@outlook.com>
2020-04-03 20:18:30 -0300

    Iniciando desenv de teste

--- src/test/java/br/com/alura/forum/ForumApplicationTests.java
+++ src/test/java/br/com/alura/forum/ForumApplicationTests.java
@@ -1,13 +1,25 @@
 package br.com.alura.forum;
 
+import br.com.alura.forum.security.service.UserService;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.test.context.junit4.SpringRunner;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+@RunWith(SpringRunner.class)
 @SpringBootTest
 class ForumApplicationTests {
 
+	@Autowired
+	private UserService userService;
+
 	@Test
 	void contextLoads() {
+		assertThat(userService).isNotNull();
+		assertThat(userService).isInstanceOf(UserDetailsService.class);
 	}
-
 }
