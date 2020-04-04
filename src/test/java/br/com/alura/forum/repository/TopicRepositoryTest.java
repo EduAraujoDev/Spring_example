a3c53e5ca3d0246cca3fd6dd3c88a1ce443160b5
Eduardo Araújo <eduardo.araujo0@outlook.com>
2020-04-04 09:44:26 -0300

    Testado os topicos abertos por categoria com h2

--- src/test/java/br/com/alura/forum/repository/TopicRepositoryTest.java
+++ src/test/java/br/com/alura/forum/repository/TopicRepositoryTest.java
@@ -1,6 +1,8 @@
 package br.com.alura.forum.repository;
 
+import br.com.alura.forum.model.OpenTopicsByCategory;
 import br.com.alura.forum.model.topic.domain.Topic;
+import br.com.alura.forum.repository.setup.TopicRepositoryTestSetup;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,6 +10,8 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -31,5 +35,22 @@ public class TopicRepositoryTest {
 
         assertThat(topicoEncontrado).isNotNull();
         assertThat(topicoEncontrado.getShortDescription()).isEqualTo(persistedTopic.getShortDescription());
+    }
+
+    @Test
+    public void shouldReturnOpenTopicsByCategory() {
+        TopicRepositoryTestSetup testSetup = new TopicRepositoryTestSetup(testEntityManager);
+        testSetup.openTopicsByCategorySetup();
+
+        List<OpenTopicsByCategory> openTopics = this.topicRepository.findOpenTopicsByCategory();
+
+        assertThat(openTopics).isNotEmpty();
+        assertThat(openTopics).hasSize(2);
+
+        assertThat(openTopics.get(0).getCategoryName()).isEqualTo("Programação");
+        assertThat(openTopics.get(0).getTopicCount()).isEqualTo(2);
+
+        assertThat(openTopics.get(1).getCategoryName()).isEqualTo("Front-end");
+        assertThat(openTopics.get(1).getTopicCount()).isEqualTo(1);
     }
 }
