68e3b0a3ff29b7dfe781a6baee0d0c1ca4a61dbd
Eduardo Araújo <eduardo.araujo0@outlook.com>
2020-04-04 09:51:55 -0300

    Alterado de h2 para MySql o banco dos testes

--- src/test/java/br/com/alura/forum/ForumApplicationTests.java
+++ src/test/java/br/com/alura/forum/ForumApplicationTests.java
@@ -6,12 +6,14 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@ActiveProfiles("test")
 class ForumApplicationTests {
 
 	@Autowired
--- src/test/java/br/com/alura/forum/repository/TopicRepositoryTest.java
+++ src/test/java/br/com/alura/forum/repository/TopicRepositoryTest.java
@@ -6,6 +6,7 @@ import br.com.alura.forum.repository.setup.TopicRepositoryTestSetup;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.test.context.ActiveProfiles;
@@ -17,6 +18,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 @RunWith(SpringRunner.class)
 @DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 @ActiveProfiles("test")
 public class TopicRepositoryTest {
 
